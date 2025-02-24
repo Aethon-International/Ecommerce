c4a40c2eed966a57399ad4e42686917286057518
safeer <safeer.435612@gmail.com>
2025-02-24 12:28:20 +0500

    just comments pass for push code verify

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,9 @@ Route::get('/orders', [homecontroller::class, 'orders']);
 //cancel order 
 Route::get('/remove/orders/{id}', [homecontroller::class, 'remove_orders']);
 Route::get('/search', [homecontroller::class, 'product_search']);
+// for verify pushing code
+
+
 
 
 
